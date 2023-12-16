f2bc75229538c7f5eee0cea6377569622a146e83
dzkm <13489382+dzkm@users.noreply.github.com>
2023-12-16 19:18:49 -0300

    feat: notifier dropdown carteira tela lançamento

---
+++ lib/Providers/View/Entrada/entrada_carteira_notifier.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:dinkid_mobile/Connections/Database/database.dart';
+
+class EntradaCarteiraNotifier extends StateNotifier<CarteirasCompanion> {
+  EntradaCarteiraNotifier(CarteirasCompanion categoria) : super(categoria);
+
+  void updateCategory(CarteirasCompanion categoria) {
+    state = categoria;
+  }
+}

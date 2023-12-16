02a8313e90a7496264002780f4a1c46571b437a2
dzkm <13489382+dzkm@users.noreply.github.com>
2023-12-16 18:43:49 -0300

    feat(compras): adicionado model de compras e parcelas

---
+++ lib/Model/Compras/compras.dart
@@ -0,0 +1,14 @@
+// @dart=2.17
+import 'package:drift/drift.dart';
+
+@DataClassName('Compra')
+class Compras extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get descricao => text().withLength(min: 2, max: 32)();
+  IntColumn get numero_parcelas => integer()();
+  DateTimeColumn get createdAt =>
+      dateTime().withDefault(Constant(DateTime.now()))();
+  DateTimeColumn get updatedAt => dateTime()();
+  BoolColumn get isDeleted => boolean().withDefault(Constant(false))();
+  DateTimeColumn get deletedAt => dateTime()();
+}
---
+++ lib/Model/Compras/compras_parcelas.dart
@@ -0,0 +1,18 @@
+// @dart=2.17
+import 'package:drift/drift.dart';
+import 'package:dinkid_mobile/src/Model/Compras/compras.dart';
+import 'package:dinkid_mobile/src/Model/Movimentacoes/movimentacoes.dart';
+
+@DataClassName('CompraParcela')
+class ComprasParcelas extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get id_compra => integer().references(Compras, #id)();
+  IntColumn get numero_parcela => integer()();
+  IntColumn get id_movimentacao => integer().references(Movimentacoes, #id)();
+  DateTimeColumn get data_prevista => dateTime()();
+  DateTimeColumn get createdAt =>
+      dateTime().withDefault(Constant(DateTime.now()))();
+  DateTimeColumn get updatedAt => dateTime()();
+  BoolColumn get isDeleted => boolean().withDefault(Constant(false))();
+  DateTimeColumn get deletedAt => dateTime()();
+}

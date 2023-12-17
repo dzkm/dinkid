6ea8536f2af32050133c1495c553c40a435d3883
dzkm <13489382+dzkm@users.noreply.github.com>
2023-12-17 16:36:17 -0300

    feat: adicionado deleteByAutoIncrement em CategoriasRepository

--- lib/Providers/Models/Repositories/Usuario/Config/CategoriasRepository.dart
+++ lib/Providers/Models/Repositories/Usuario/Config/CategoriasRepository.dart
@@ -8,6 +8,16 @@ class CategoriasRepository
   final AppDatabase _database;
 
   CategoriasRepository(this._database) : super(_database, _database.categorias);
+
+  Future<int> deleteByAutoIncrement(int id) async {
+    var idColumn =
+        this.table.$columns.firstWhere((column) => column.hasAutoIncrement);
+    var ghostCompanion = CategoriasCompanion(
+        isDeleted: Value(true), deletedAt: Value(DateTime.now()));
+    return await (_database.update(this.table)
+          ..where((tbl) => idColumn.equals(id)))
+        .write(ghostCompanion);
+  }
 }
 
 final categoriasRepositoryProvider = Provider<CategoriasRepository>((ref) {

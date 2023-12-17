6c508e593096c65305e9d6a1580a3df3d23cedda
dzkm <13489382+dzkm@users.noreply.github.com>
2023-12-17 13:32:03 -0300

    feat: adicionado as tabelas novas no drift

--- lib/Connections/Database/database.dart
+++ lib/Connections/Database/database.dart
@@ -4,13 +4,26 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'package:dinkid_mobile/Model/Movimentacoes/movimentacoes.dart';
-import 'package:dinkid_mobile/Model/Usuario/usuario.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:dinkid_mobile/Model/Compras/compras.dart';
+import 'package:dinkid_mobile/Model/Compras/compras_parcelas.dart';
+import 'package:dinkid_mobile/Model/Movimentacoes/movimentacoes.dart';
+import 'package:dinkid_mobile/Model/Movimentacoes/tipos_movimentacoes.dart';
+import 'package:dinkid_mobile/Model/Usuario/usuario.dart';
+import 'package:dinkid_mobile/Model/Usuario/Config/carteiras.dart';
+import 'package:dinkid_mobile/Model/Usuario/Config/categorias.dart';
 
 part 'database.g.dart';
 
-@DriftDatabase(tables: [Movimentacoes, Usuarios])
+@DriftDatabase(tables: [
+  Movimentacoes,
+  Usuarios,
+  TiposMovimentacoes,
+  Carteiras,
+  Categorias,
+  Compras,
+  ComprasParcelas
+])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
   @override

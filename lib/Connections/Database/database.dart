bf6f11b4302880e5584abcb4ce7c2670f263ac04
dzkm <13489382+dzkm@users.noreply.github.com>
2023-12-17 22:43:20 -0300

    feat: adicionado recomendações das docs no Drift

--- lib/Connections/Database/database.dart
+++ lib/Connections/Database/database.dart
@@ -12,6 +12,8 @@ import 'package:dinkid_mobile/Model/Movimentacoes/tipos_movimentacoes.dart';
 import 'package:dinkid_mobile/Model/Usuario/usuario.dart';
 import 'package:dinkid_mobile/Model/Usuario/Config/carteiras.dart';
 import 'package:dinkid_mobile/Model/Usuario/Config/categorias.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 part 'database.g.dart';
 
@@ -27,13 +29,26 @@ part 'database.g.dart';
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
   @override
-  int get schemaVersion => 4;
+  int get schemaVersion => 2;
 }
 
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
     final file = File(p.join(dbFolder.path, 'dinkid.db'));
+
+    // Also work around limitations on old Android versions
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+
+    // Make sqlite3 pick a more suitable location for temporary files - the
+    // one from the system may be inaccessible due to sandboxing.
+    final cachebase = (await getTemporaryDirectory()).path;
+    // We can't access /tmp on Android, which sqlite3 would try by default.
+    // Explicitly tell it about the correct temporary directory.
+    sqlite3.tempDirectory = cachebase;
+
     return NativeDatabase.createInBackground(file);
   });
 }
